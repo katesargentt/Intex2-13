1214d5e9618e997e547c3656f043e0017d6f2955
arossiter2 <ainsleyrossiter@gmail.com>
2025-04-08 19:51:19 -0600

    Fixed the DetailPage, got it to show up

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -5,6 +5,8 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 import './DetailPage.css';
+import AuthorizeView, { AuthorizedUser } from '../components/AuthorizeView';
+import Logout from '../components/Logout';
 
 // const API_URL =
 //   'https://cineniche-2-13-backend-f9bef5h7ftbscahz.eastus-01.azurewebsites.net/api/Movie';
@@ -14,13 +16,17 @@ const API_URL = 'https://localhost:5000/api/Movie';
 const IMAGE_URL =
   'https://cinenicheimages.blob.core.windows.net/movieposters/Movie Posters/Movie Posters';
 
+// const endpoint = `${API_URL}/recommend/user/3`;
+
 function DetailPage() {
   const { showId } = useParams();
   const [movie, setMovie] = useState<Movie | null>(null);
   const [imageError, setImageError] = useState(false);
 
   useEffect(() => {
-    fetch(`${API_URL}/${showId}`)
+    fetch(`${API_URL}/${showId}`, {
+      credentials: 'include',
+    })
       .then(async (res) => {
         if (!res.ok) {
           throw new Error(`HTTP error! status: ${res.status}`);
@@ -40,56 +46,63 @@ function DetailPage() {
 
   return (
     <>
-      <Header />
-      <div className="detail-container">
-        {!imageError ? (
-          <img
-            src={`${IMAGE_URL}/${encodeURIComponent(movie.title.trim())}.jpg`}
-            alt={movie.title}
-            className="poster-image"
-            onError={() => setImageError(true)}
-          />
-        ) : (
-          <div className="poster-fallback">
-            <span>{movie.title}</span>
+      <AuthorizeView>
+        <span>
+          <Logout>
+            Logout <AuthorizedUser value="email" />
+          </Logout>
+        </span>
+        <Header />
+        <div className="detail-container">
+          {!imageError ? (
+            <img
+              src={`${IMAGE_URL}/${encodeURIComponent(movie.title.trim())}.jpg`}
+              alt={movie.title}
+              className="poster-image"
+              onError={() => setImageError(true)}
+            />
+          ) : (
+            <div className="poster-fallback">
+              <span>{movie.title}</span>
+            </div>
+          )}
+
+          <div className="movie-details">
+            <h1>{movie.title}</h1>
+            <p className="description">{movie.description}</p>
+            <p>
+              <strong>Year:</strong> {movie.releaseYear}
+            </p>
+            <p>
+              <strong>Duration:</strong> {movie.duration}
+            </p>
+            <p>
+              <strong>Rating:</strong> {movie.rating}
+            </p>
+            <p>
+              <strong>Director:</strong> {movie.director}
+            </p>
+            <p>
+              <strong>Cast:</strong> {movie.cast}
+            </p>
+            <p>
+              <strong>Country:</strong> {movie.country}
+            </p>
+            <p>
+              <strong>Genres:</strong>{' '}
+              {movie.categories && movie.categories.length > 0
+                ? movie.categories.join(', ')
+                : 'N/A'}
+            </p>
           </div>
-        )}
-
-        <div className="movie-details">
-          <h1>{movie.title}</h1>
-          <p className="description">{movie.description}</p>
-          <p>
-            <strong>Year:</strong> {movie.releaseYear}
-          </p>
-          <p>
-            <strong>Duration:</strong> {movie.duration}
-          </p>
-          <p>
-            <strong>Rating:</strong> {movie.rating}
-          </p>
-          <p>
-            <strong>Director:</strong> {movie.director}
-          </p>
-          <p>
-            <strong>Cast:</strong> {movie.cast}
-          </p>
-          <p>
-            <strong>Country:</strong> {movie.country}
-          </p>
-          <p>
-            <strong>Genres:</strong>{' '}
-            {movie.categories && movie.categories.length > 0
-              ? movie.categories.join(', ')
-              : 'N/A'}
-          </p>
         </div>
-      </div>
-      <div>
-        <h4>If you liked {movie.title}...</h4>
-        <p>future recommender here</p>
-      </div>
+        <div>
+          <h4>If you liked {movie.title}...</h4>
+          <p>future recommender here</p>
+        </div>
 
-      <Footer />
+        <Footer />
+      </AuthorizeView>
     </>
   );
 }
